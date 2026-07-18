750aa2baccf57ba9a79165b56d9585dc4f528377
negiyugang27-svg <negiyugang27@gmail.com>
2026-07-18 10:10:33 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first number"));
+b=int(input("Enter second number"));
+print("The sum of",a,"and",b,"is",a+b);
+print("The difference of",a,"and",b,"is",a-b);
+print("The product of",a,"and",b,"is",a*b);
+print("The quotient of",a,"and",b,"is",a/b);
